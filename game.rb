c5b82e75575f0be4aa548e46cd1690af608f3ec1
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-06 22:06:55 +0500

    Add the Author class

--- game.rb
+++ game.rb
@@ -1,11 +1,13 @@
 class Game < Item 
+    attr_accessor :multiplayer, :last_played_at
+    
     def initialize(multiplayer, last_played_at)
         @multiplayer = multiplayer
         @last_played_at = last_played_at
     end
 
     def can_be_archived?
-        if super && (Time.now - last_played_at > 2 * 365 * 24 * 60 * 60)
+        if super && (Time.now - @last_played_at > 2 * 365 * 24 * 60 * 60)
             true
         else
             false

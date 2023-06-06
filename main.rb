9944d89502b35e58ed8911cac17d100656cb7be9
Swarzstein <zorrillagja@gmail.com>
2023-06-06 12:13:59 -0400

    menu ready

--- main.rb
+++ main.rb
@@ -1,49 +1,22 @@
-class Main
-    OPTIONS = {
-        1 => :select_books
-        2 => :select_music
-        3 => :select_games
-        0 => :exit_app
-    }
-    
-    def initialize(app)
-        @app = app
-    end
+# frozen_string_literal: true
 
-    def display_menu_options
-        puts "Please selelct an option:"
-        OPTIONS.each do |key, value|
-            puts "#{key}: #{menu_option_name(value)}"
-        end
-    end
+require_relative 'modules/menu'
+require_relative 'app'
 
-    def handle_choice(choice)
-        if OPTIONS.keys?(choice)
-            send(OPTIONS[choice])
-        else
-            puts "Invalid choice, please try again."
-        end
-    end
+def main
+  app = App.new
+  menu = Menu.new(app)
 
-    private
+  # app.load_data
 
-    def menu_option_name(method_name)
-        method_name.to_s.split("_").map(&:capitalize).join(" ")
-    end
+  loop do
+    break if menu.display_menu_options == 4
+  end
 
-    def select_books
-        @app.select_books
-    end
-
-    def select_music
-        @app.select_music
-    end
-
-    def select_games
-        @app.select_games
-    end
-
-    def exit_app
-        puts "Exiting the app, goodbye!"
-    end
+  # app.save_data
+  puts 'Exiting the app, goodbye!'
+  gets
+  app.clear_screen
 end
+
+main

64799fe6a1545f438556a232e4ee81d592f92acf
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-06-07 20:33:01 +0500

    Update author.rb

--- author.rb
+++ author.rb
@@ -1,6 +1,6 @@
 class Author
-    attr_accessor :first_name, :last_name, :items
-    attr_reader :id
+    attr_accessor :first_name, :last_name
+    attr_reader :id, :items
     def initialize(first_name, last_name)
         @id = Random.rand(1..1000)
         @first_name = first_name
